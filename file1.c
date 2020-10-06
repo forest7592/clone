a630f822455f92871b8a582a07930934ec5e6524
Owner <forest7592@hanmail.net>
2020-10-06 20:39:32 +0900

    복제 실습

---
+++ file1.c
@@ -0,0 +1,9 @@
+//복제 실습 파일1
+#include<stdio.h>
+
+int main()
+{
+	printf("Hello World!!");
+	printf("Hello World2!!");
+	return 0;
+}
---
+++ file2.c
@@ -0,0 +1,9 @@
+//복제 실습 파일2
+#include<stdio.h>
+
+int main()
+{
+	printf("Hello World!!");
+	printf("Hello World2!!");
+	return 0;
+}
